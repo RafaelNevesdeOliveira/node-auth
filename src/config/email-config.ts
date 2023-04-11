38498bf6e3d3a75a09cd759096eae9920fb22890
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2023-04-10 23:41:45 -0300

    up

--- src/config/email-config.ts
+++ src/config/email-config.ts
@@ -1,8 +1,9 @@
 class Configs {
   public host = "smtp.gmail.com";
   public port = 587;
-  public user = "";
-  public password = "";
+  public user = "entidadedoamorporxande@gmail.com";
+  public password = "nsdionnppvjajcof";
+  // public password = "entidadedoamor@2023";
 }
 
 export default new Configs;
